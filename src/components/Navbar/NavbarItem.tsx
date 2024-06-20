bf12dfd88d9976b764656db5cd3940fc98817ee6
bricekams <kamhouabrice0@gmail.com>
2024-06-20 19:34:51 +0100

    fix unsued

--- src/components/Navbar/NavbarItem.tsx
+++ src/components/Navbar/NavbarItem.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useEffect, useRef, useState } from "react";
+import React, { FC, useRef, useState } from "react";
 import { MdArrowRight } from "react-icons/md";
 
 interface MenuItem {
@@ -16,7 +16,7 @@ interface Props {
   children: MenuItem[];
 }
 
-const NavbarItem: FC<Props> = ({ title, description, image, children }) => {
+const NavbarItem: FC<Props> = ({ title, children }) => {
   const [showSubs, setShowSubs] = useState(false);
   const [hovered, setHovered] = useState(0);
   const [selected, setSelected] = useState<MenuItem | undefined>(undefined);
@@ -36,79 +36,81 @@ const NavbarItem: FC<Props> = ({ title, description, image, children }) => {
         {title}
       </div>
       {showSubs && (
-        <div
-          onMouseEnter={() => {
-            if (timeOutRef.current) {
-              clearTimeout(timeOutRef.current);
-            }
-            setShowSubs(true);
-          }}
-          onMouseLeave={() => {
-            timeOutRef.current = setTimeout(() => {
-              setShowSubs(false);
-            }, 200);
-          }}
-          className="absolute mt-5 bg-white shadow-xl z-10 w-full left-0 x-padding right-0 flex justify-between"
-        >
-          <div className="min-w-[25%] border-r border-r-gray-100">
-            {
-              <ul className="flex flex-col">
-                {children.map((c, i) => {
-                  const hasChild = (c.links?.length ?? -1) > 0;
+        <div className="x-padding absolute z-10 w-full  left-0 right-0">
+          <div
+            onMouseEnter={() => {
+              if (timeOutRef.current) {
+                clearTimeout(timeOutRef.current);
+              }
+              setShowSubs(true);
+            }}
+            onMouseLeave={() => {
+              timeOutRef.current = setTimeout(() => {
+                setShowSubs(false);
+              }, 200);
+            }}
+            className=" mt-5 bg-white shadow-xl w-full flex justify-between"
+          >
+            <div className="min-w-[25%] border-r border-r-gray-100">
+              {
+                <ul className="flex flex-col">
+                  {children.map((c, i) => {
+                    const hasChild = (c.links?.length ?? -1) > 0;
 
+                    return (
+                      <li
+                        key={i}
+                        onMouseEnter={() => {
+                          setHovered(i);
+                          setSelected(c);
+                        }}
+                        className={`flex justify-between items-center cursor-pointer  ${i === hovered ? "text-primary bg-gray-100" : "text-pale"} px-4 ${i === children.length - 1 ? "" : ""} py-[10px]`}
+                      >
+                        <a
+                          href={c.href ?? "#"}
+                          className={`${hasChild ? "pointer-events-none" : ""} h-full w-full`}
+                        >
+                          {c.name}
+                        </a>
+                        {hasChild && (
+                          <div>
+                            <MdArrowRight size={18} />
+                          </div>
+                        )}
+                      </li>
+                    );
+                  })}
+                </ul>
+              }
+            </div>
+            {(selected?.links?.length ?? -1) > 0 && (
+              <ul className="w-full">
+                {selected?.links?.map((l, i) => {
                   return (
                     <li
                       key={i}
-                      onMouseEnter={() => {
-                        setHovered(i);
-                        setSelected(c);
-                      }}
-                      className={`flex justify-between items-center cursor-pointer  ${i === hovered ? "text-primary bg-gray-100" : "text-pale"} px-4 ${i === children.length - 1 ? "" : ""} py-[10px]`}
+                      className="w-full flex justify-between  cursor-pointer hover:text-primary hover:bg-gray-100 text-pale px-4 py-[10px]"
                     >
-                      <a
-                        href={c.href ?? "#"}
-                        className={`${hasChild ? "pointer-events-none" : ""} h-full w-full`}
-                      >
-                        {c.name}
+                      <a href={l.href ?? "#"} className="w-full">
+                        {l.name}
                       </a>
-                      {hasChild && (
-                        <div>
-                          <MdArrowRight size={18} />
-                        </div>
-                      )}
                     </li>
                   );
                 })}
               </ul>
-            }
-          </div>
-          {(selected?.links?.length ?? -1) > 0 && (
-            <ul className="w-full">
-              {selected?.links?.map((l, i) => {
-                return (
-                  <li
-                    key={i}
-                    className="w-full flex justify-between  cursor-pointer hover:text-primary hover:bg-gray-100 text-pale px-4 py-[10px]"
-                  >
-                    <a href={l.href ?? "#"} className="w-full">
-                      {l.name}
-                    </a>
-                  </li>
-                );
-              })}
-            </ul>
-          )}
-          {(selected?.links?.length ?? -1) < 0 && (
-            <div className="px-4 py-2 flex justify-start w-full">
-              <div>
-                <h1 className="text-black font-bold mb-3">
-                  {selected?.description?.title}
-                </h1>
-                <p className="text-pale">{selected?.description?.content}</p>
+            )}
+            {(selected?.links?.length ?? -1) < 0 && (
+              <div className="px-4 py-2 flex justify-start w-full">
+                <div>
+                  <h1 className="text-black font-bold mb-3">
+                    {selected?.description?.title}
+                  </h1>
+                  <p className="text-pale">{selected?.description?.content}</p>
+                </div>
               </div>
-            </div>
-          )}
-          <div className="min-w-[324px] min-h-[324px] bg-pale"></div>
+            )}
+            <div className="min-w-[324px] min-h-[324px] bg-pale"></div>
+          </div>
         </div>
       )}
     </div>
